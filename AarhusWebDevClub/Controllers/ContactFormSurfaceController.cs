128fe7edcd90a1e477a1eb26c37d76c1629a9324
Jannick S. Pedersen <44465341+jannicksp@users.noreply.github.com>
2021-05-03 11:08:41 +0200

    Add project files.

---
+++ AarhusWebDevClub/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevClub.ViewModels;
+using Umbraco.Core.Models;
+
+
+namespace AarhusWebDevClub.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm" , new ContactForm());
+        }
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+               return CurrentUmbracoPage();
+
+            // Saving data to Umbraco Backend
+            IContent comment = Services.ContentService.Create(model.Subject, CurrentPage.Id, "comment");
+            comment.SetValue("username", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("message", model.Message);
+            Services.ContentService.Save(comment);
+
+
+            TempData["success!"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
